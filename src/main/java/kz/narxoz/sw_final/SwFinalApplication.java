09a0fcbbdd1d30cd9ea96f4f597a56a0d4b5d53b
Nazarbek <kamdswe@icloud.com>
2025-12-26 23:15:56 +0500

    chore: bootstrap spring boot project

---
+++ src/main/java/kz/narxoz/sw_final/SwFinalApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.sw_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/narxoz/sw_final/SwFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.sw_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
