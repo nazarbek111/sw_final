a747440ae0519e110024ee2d468df9a8961fbaa0
Nazarbek <kamdswe@icloud.com>
2025-12-26 23:47:17 +0500

    feat: add dto mappers

---
+++ src/main/java/kz/narxoz/sw_final/mapper/StudentMapper.java
@@ -0,0 +1,20 @@
+package kz.narxoz.sw_final.mapper;
+
+import kz.narxoz.sw_final.dto.StudentDto;
+import kz.narxoz.sw_final.entity.Student;
+
+public class StudentMapper {
+    public static StudentDto toDto(Student s) {
+        return new StudentDto(s.getId(), s.getFirstName(), s.getLastName(), s.getEmail(), s.getAge());
+    }
+
+    public static Student toEntity(StudentDto d) {
+        Student s = new Student();
+        s.setId(d.getId());
+        s.setFirstName(d.getFirstName());
+        s.setLastName(d.getLastName());
+        s.setEmail(d.getEmail());
+        s.setAge(d.getAge());
+        return s;
+    }
+}
---
+++ src/main/java/kz/narxoz/sw_final/mapper/TeacherMapper.java
@@ -0,0 +1,19 @@
+package kz.narxoz.sw_final.mapper;
+
+import kz.narxoz.sw_final.dto.TeacherDto;
+import kz.narxoz.sw_final.entity.Teacher;
+
+public class TeacherMapper {
+    public static TeacherDto toDto(Teacher t) {
+        return new TeacherDto(t.getId(), t.getFirstName(), t.getLastName(), t.getEmail());
+    }
+
+    public static Teacher toEntity(TeacherDto d) {
+        Teacher t = new Teacher();
+        t.setId(d.getId());
+        t.setFirstName(d.getFirstName());
+        t.setLastName(d.getLastName());
+        t.setEmail(d.getEmail());
+        return t;
+    }
+}
